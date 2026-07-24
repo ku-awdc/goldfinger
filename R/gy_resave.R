bc2157a3f5644edf2d4a9487a902a51c33888f2f
Matt Denwood <md@sund.ku.dk>
2026-07-24 10:24:03 +0200

    Write qs vignette

---
+++ R/gy_resave.R
@@ -0,0 +1,16 @@
+#' Upgrade older goldfinger saved data files to remove dependencies on the qs package
+#'
+#' @details
+#' For more details see vignette("goldfinger-qs")
+#'
+#' @param input_file the name of the saved file to resave
+#' @param output_file the name of the file to save to (default is to replace the existing file)
+#'
+#' @export
+gy_resave <- function(input_file=stop("input_file must be specified (.rdg file extension is recommended)"), output_file=input_file){
+
+  fcon <- readRDS(file)
+
+  stop("TODO")
+
+}
